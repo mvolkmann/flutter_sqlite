b0eaec9e46f3018aefd63f3b0fde66c458126ab9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-07 17:01:24 -0600

    initial commit

---
+++ lib/dog.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+
+class Dog {
+  int id = 0;
+  int age;
+  String breed;
+  String name;
+
+  Dog({
+    this.id,
+    required this.age,
+    required this.breed,
+    required this.name,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'age': age, 'breed': breed, 'name': name};
+  }
+
+  // For debugging
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, breed: $breed, age: $age}';
+  }
+}
+
+Future<void> insertDog(Database database, Dog dog) async {
+  final db = database;
+  await db.insert(
+    'dogs',
+    dog.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+Future<List<Dog>> getDogs(Database database) async {
+  final db = database;
+  final List<Map<String, dynamic>> maps = await db.query('dogs');
+  return List.generate(maps.length, (index) {
+    var map = maps[index];
+    return Dog(
+      id: map['id'],
+      age: map['age'],
+      breed: map['breed'],
+      name: map['name'],
+    );
+  });
+}
+
+Future<void> updateDog(Database database, Dog dog) async {
+  final db = database;
+  await db.update(
+    'dogs',
+    dog.toMap(),
+    where: 'id = ?',
+    // This prevents SQL injection.
+    whereArgs: [dog.id],
+  );
+}
+
+Future<void> deleteDog(Database database, int id) async {
+  final db = database;
+  await db.delete(
+    'dogs',
+    where: 'id = ?',
+    whereArgs: [id],
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import './dog.dart';
+
+const title = 'My App';
+
+void main() => runApp(
+      MaterialApp(
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Home(),
+      ),
+    );
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Database database;
+
+  @override
+  void initState() {
+    super.initState();
+
+    dbSetup();
+  }
+
+  void dbSetup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    database = await openDatabase(
+      join(await getDatabasesPath(), 'dog.db'),
+      onCreate: (db, version) {
+        //TODO: What does this do if the table already exists?
+        //TODO: Will this automatically assign auto-incrementing ids?
+        return db.execute(
+          'create table dogs('
+          'id integer primary key, age integer, breed text, name text)',
+        );
+      },
+      // The version provides a path to perform database upgrades and downgrades.
+      version: 1,
+    );
+  }
+
+  void demo() async {
+    var comet = Dog(name: 'Comet', breed: 'Whippet', age: 1);
+    await insertDog(database, comet);
+
+    var dogs = await getDogs(database);
+    print('main.dart demo: initial dogs = $dogs');
+
+    comet.age += 1;
+    await updateDog(database, comet);
+
+    await deleteDog(database, comet.id);
+
+    dogs = await getDogs(database);
+    print('main.dart demo: final dogs = $dogs');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Add content here.'),
+          ],
+        ),
+      ),
+    );
+  }
+}
