3f2ae68411739061dc996dccc146a498dff28134
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-11 16:16:10 -0600

    progress

--- lib/dog_list.dart
+++ lib/dog_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import './dog.dart';
 import './dog_form.dart';
-import './extensions/widget_extensions.dart';
 
 class DogList extends StatefulWidget {
   final List<Dog> dogs;
@@ -23,32 +22,48 @@ class _DogListState extends State<DogList> {
   @override
   Widget build(BuildContext context) {
     var dogs = widget.dogs;
-    return ListView.separated(
-      itemBuilder: (_, index) => _buildTile(dogs[index]),
-      separatorBuilder: (_, index) => Divider(thickness: 1),
-      itemCount: dogs.length,
-    ).expanded;
+    dogs.sort((d1, d2) => d1.name.compareTo(d2.name));
+
+    return Expanded(
+      child: ListView.separated(
+        itemBuilder: (_, index) => _buildTile(dogs[index]),
+        itemCount: dogs.length,
+        /* When using ReorderableListView.builder ...
+        onReorder: (oldIndex, newIndex) {
+          if (newIndex > oldIndex) newIndex--;
+          setState(() {
+            var dog = dogs.removeAt(oldIndex);
+            dogs.insert(newIndex, dog);
+          });
+        },
+        */
+        separatorBuilder: (_, index) => Divider(thickness: 1),
+      ),
+    );
   }
 
   Widget _buildTile(Dog dog) {
     var subtitle = '${dog.breed} - age ${dog.age}';
     return ListTile(
-        title: Text(dog.name),
-        subtitle: Text(subtitle),
-        trailing: IconButton(
-          icon: Icon(Icons.delete, color: Colors.red),
-          onPressed: () => widget.onDelete(dog),
+      title: Text(dog.name),
+      subtitle: Text(subtitle),
+      trailing: IconButton(
+        icon: Icon(Icons.delete, color: Colors.red),
+        onPressed: () => widget.onDelete(dog),
+      ),
+      onTap: () => _goToForm(dog),
+    );
+  }
+
+  void _goToForm(Dog dog) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DogForm(
+          dog: dog,
+          onUpdate: widget.onUpdate,
         ),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => DogForm(
-                dog: dog,
-                onUpdate: widget.onUpdate,
-              ),
-            ),
-          );
-        });
+      ),
+    );
   }
 }
